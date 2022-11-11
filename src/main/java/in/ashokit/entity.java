54f79b5afa8f865f60d19791264239574c68edc3
sha02shi92 <98804064+sha02shi92@users.noreply.github.com>
2022-11-11 20:12:45 +0530

    Update entity.java

--- src/main/java/in/ashokit/entity.java
+++ src/main/java/in/ashokit/entity.java
@@ -5,6 +5,7 @@ public class entity {
 	public static void main(String[] args) {
 		
 		System.out.println("shashank Dewanand Gajbhiye");
+		System.out.println("Shashank Updated the List");
 		
 	}
 	
